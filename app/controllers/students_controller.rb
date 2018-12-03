176c6de00a836166afbb0bc6e643e9d9aae1d25c
Andrew Jones <joneseyit@protonmail.com>
2018-12-03 14:29:01 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,35 @@
 class StudentsController < ApplicationController
-  
+before_action :student_params, only: :show
   def index
     @students = Student.all
   end
 
   def show
+
     @student = Student.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+
+  end
+
+  private
+    def student_params
+      # params.require(:article).permit(:title, :text)
+      params.permit("first_name", "last_name")
+
+
+    end
 
 end
